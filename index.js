209803e53b265dc15bf54c37503ceaf40ee388ff
justinfinn2891 <jfinn5@ut.utm.edu>
2025-01-29 00:07:37 -0600

    yes

---
+++ index.js
@@ -0,0 +1,14 @@
+
+document.getElementById("yes-button").onclick = function()
+{
+    console.log("work");
+    document.getElementById("style-class").style.display = "none";
+
+}
+
+document.getElementById("no-button").onclick = function()
+{
+    console.log("work");
+    document.getElementById("no-button").style.display = "none";
+    
+}
